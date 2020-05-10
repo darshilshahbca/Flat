c0f403e568c6795a6a2c86506727bd34cccfa72d
darshilshahbca <darshil.saphcm@gmail.com>
2020-05-10 21:17:31 +0530

    - Login Added with Remember Me.

---
+++ app/src/main/java/com/example/flat/MainActivity.java
@@ -0,0 +1,140 @@
+package com.example.flat;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.flat.Common.Common;
+import com.example.flat.Model.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.rengwuxian.materialedittext.MaterialEditText;
+import com.rey.material.widget.CheckBox;
+
+import io.paperdb.Paper;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edtPhone, edtPassword;
+    Button btnSignIn;
+
+    CheckBox ckbRemember;
+
+    FirebaseDatabase database;
+    DatabaseReference table_user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtPassword = (MaterialEditText)findViewById(R.id.edtPassword);
+        edtPhone = (MaterialEditText)findViewById(R.id.edtPhone);
+        btnSignIn = (Button)findViewById(R.id.btnSignIn);
+        ckbRemember = (CheckBox)findViewById(R.id.ckbRemember);
+
+        //Init Pager
+        Paper.init(this);
+
+        //Init Firebase
+        database = FirebaseDatabase.getInstance();
+        table_user = database.getReference("User");
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (Common.isConnectedToInternet(getBaseContext())) {
+
+                    //Save User & password
+                    if(ckbRemember.isChecked())
+                    {
+                        Paper.book().write(Common.USER_KEY, edtPhone.getText().toString());
+                        Paper.book().write(Common.PWD_KEY,edtPassword.getText().toString());
+                    }
+
+                    login(edtPhone.getText().toString(),edtPassword.getText().toString());
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Please check your connection!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+        });
+
+        //Check Remember
+        String user = Paper.book().read(Common.USER_KEY);
+        String pwd = Paper.book().read(Common.PWD_KEY);
+        if(user !=null && pwd != null){
+            if(!user.isEmpty() && !pwd.isEmpty())
+            {
+                login(user,pwd);
+            }
+        }
+    }
+
+    private void login(final String phone, final String pwd) {
+        //Init Firebase
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference table_user = database.getReference("User");
+
+        if (Common.isConnectedToInternet(getBaseContext())) {
+
+            final ProgressDialog mDialog = new ProgressDialog(MainActivity.this);
+            mDialog.setMessage("Please Wait....");
+            mDialog.show();
+
+            table_user.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                    //Check if User not exist in Database
+                    if (dataSnapshot.child(phone).exists()) {
+
+
+                        mDialog.dismiss();
+
+                        //Get User Information
+                        User user = dataSnapshot.child(phone).getValue(User.class);
+                        user.setPhone(phone); //Set Phone Number
+
+                        if (user.getPassword().equals(pwd)) {
+                            Intent homeIntent = new Intent(MainActivity.this, Home.class);
+
+                            //To Store Login User Detail
+                            Common.currentUser = user;
+                            startActivity(homeIntent);
+                            finish();
+
+                        } else {
+                            Toast.makeText(MainActivity.this, "Wrong Password !", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        mDialog.dismiss();
+                        Toast.makeText(MainActivity.this, "User not exist in Database", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+        }
+        else{
+            Toast.makeText(MainActivity.this, "Please check your connection!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+    }
+}
